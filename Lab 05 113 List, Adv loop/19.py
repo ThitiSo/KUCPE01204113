495b64be081ecca0b596e68d04500d0a906e4509
ThitiSo <107989188+ThitiSo@users.noreply.github.com>
2024-07-27 13:37:00 +0700

    Update 19.py
    Task 19 Complete

--- Lab 05 113 List, Adv loop/19.py
+++ Lab 05 113 List, Adv loop/19.py
@@ -1,12 +1,13 @@
 a=input("Enter a string: ").lower().split()
 vowel=["a","e","i","o","u"]
+Lower=[ 'b', 'c', 'd', 'f', 'g', 'h',  'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
 vowelset=[]
 conset=[]
 for i in a:
     for j in i:
         if j in vowel and j not in vowelset:
             vowelset.append(j)
-        elif j not in vowel and j not in conset:
+        elif j in Lower and j not in conset:
             conset.append(j)
 
 print(f"Unique vowels:  {vowelset}\nUnique consonants:  {conset}")
