b1ef9dd2e5ee4a43ce0d1092254c07f9d88f0b73
ThitiSo <107989188+ThitiSo@users.noreply.github.com>
2024-08-07 22:20:03 +0700

    Update 09 Fill-in the 2D list.py

--- Lab 06 113 Condifitions, Loops, List 2D/09 Fill-in the 2D list.py
+++ Lab 06 113 Condifitions, Loops, List 2D/09 Fill-in the 2D list.py
@@ -1,7 +1,6 @@
 y=int(input("Enter the number of rows or columns : "))
-z=0
+
 for i in range(y):
     for j in range(y):
-        print('%2d'%(j+1+z),end=" ")
-    z+=1
+        print('%2d'%(j+1+i),end=" ")
     print("\n",end="")
